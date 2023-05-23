2a0ec16b9a543384cfb877bd294566079dc714c6
Nolan Rummel <nolan.rummel22@gmail.com>
2023-05-22 23:50:01 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    if (someValue > 42){
+        return someValue - 42
+    }
+    if (someValue < 42){
+        return 42 - someValue
+    }
+  }
+
+function distanceFromHqInFeet(someValue) {
+
+    let amountOfBlocks = distanceFromHqInBlocks(someValue)
+    
+    return amountOfBlocks * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination){
+        return (start - destination)*264
+    }
+    if (start < destination){
+        return (destination - start)*264
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+
+    let distance = distanceTravelledInFeet(start,destination)
+
+    if (distance < 400){
+        return 0
+    }
+    if (distance <= 2000){
+        return (distance - 400)*.02
+    }
+    if (distance > 2500){
+        return 'cannot travel that far'
+    }
+    if (distance > 2000){
+        return 25
+    }
+}
