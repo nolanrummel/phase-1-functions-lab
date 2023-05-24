e603acd97e2c8d0f63adcdbdd4fda6221f69fce0
Nolan Rummel <nolan.rummel22@gmail.com>
2023-05-24 14:57:08 -0400

    updated functions to use Math.abs and cleaned up syntax

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
+// function distanceFromHqInBlocks(someValue) {
+//     if (someValue > 42){
+//         return someValue - 42
+//     }
+//     if (someValue < 42){
+//         return 42 - someValue
+//     }
+//   }
+
 function distanceFromHqInBlocks(someValue) {
-    if (someValue > 42){
-        return someValue - 42
-    }
-    if (someValue < 42){
-        return 42 - someValue
-    }
-  }
-
-function distanceFromHqInFeet(someValue) {
-
-    let amountOfBlocks = distanceFromHqInBlocks(someValue)
-    
-    return amountOfBlocks * 264
+    return Math.abs(someValue - 42)
 }
 
-function distanceTravelledInFeet(start, destination) {
-    if (start > destination){
-        return (start - destination)*264
-    }
-    if (start < destination){
-        return (destination - start)*264
-    }
+// function distanceFromHqInFeet(someValue) {
+
+//     let amountOfBlocks = distanceFromHqInBlocks(someValue)
+    
+//     return amountOfBlocks * 264
+// }
+
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue)*264
+}
+
+// function distanceTravelledInFeet(start, destination) {
+//     if (start > destination){
+//         return (start - destination)*264
+//     }
+//     if (start < destination){
+//         return (destination - start)*264
+//     }
+// }
+
+function distanceTravelledInFeet (start, destination) {
+    return Math.abs(start - destination)*264
 }
 
 function calculatesFarePrice(start, destination) {
@@ -33,10 +45,10 @@ function calculatesFarePrice(start, destination) {
     if (distance <= 2000){
         return (distance - 400)*.02
     }
+    if (distance > 2000 && distance <= 2500){
+        return 25
+    }
     if (distance > 2500){
         return 'cannot travel that far'
-    }
-    if (distance > 2000){
-        return 25
     }
 }
